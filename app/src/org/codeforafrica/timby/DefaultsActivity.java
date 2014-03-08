8a6ce0f6f2afcb57e9a5ec5736df98504f460fc9
Nick Hargreaves <hargreaves1234@gmail.com>
2014-03-08 11:51:36 +0300

    added defaults activity

---
+++ app/src/org/codeforafrica/timby/DefaultsActivity.java
@@ -0,0 +1,14 @@
+package org.codeforafrica.timby;
+
+public class DefaultsActivity {
+	public static String encrypt_zip_files = "1";
+	public static String delete_after_sync = "0";
+	public static String delete_after_export = "0";
+	public static String maximum_video_length = "60";
+	public static String encryption_key = "test";
+	public static String api_base_url = "";
+	public static String hockey_app_id = "";
+	public static String api_key = "";
+	public static String username = "";
+	public static String password = "";
+}
